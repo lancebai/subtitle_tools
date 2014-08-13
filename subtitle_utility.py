07ad1d6e03919192586ef435706749e57a37bb19
Lance Bai <lancebai@gmail.com>
2014-08-14 00:38:23 +0800

    added parse_dir. download subtitle for all the file inside the video_dir

--- subtitle_utility.py
+++ subtitle_utility.py
@@ -16,19 +16,42 @@ from g2butf8 import convertFile
 
 test_video = r'c:\dvd\Da.Vincis.Demons.S01E08.720p.HDTV.x264-EVOLVE.mkv'
 
-def main() :
-    print "main function"
-    # play_video_with_xmp(test_video)
-    xmp_downloader = XmpSubtitleDownloader(test_video)
+def parse_dir():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="file containing video clips",
+                        type=str)
+    args = parser.parse_args()
+    return args.dir
+
+
+
+def download_subtitle_in_dir(dir_name):
+    try:
+        file_list = os.listdir(dir_name)
+        for video_filename in file_list:
+            download_subtitle(dir_name+video_filename) 
+    except IOError:
+        print "failed to open directory", dir_name, 
+    
+def download_subtitle(video_filename):
+    xmp_downloader = XmpSubtitleDownloader(video_filename)
     xmp_downloader.prepare()
     xmp_downloader.download_subtitle()
     downloaded_srt = xmp_downloader.get_downloaded_subtile()
     print "subtitle downloaded : ", downloaded_srt 
     # check_subtitle_folder()
     # fileName, fileExtension = os.path.splitext(downloaded_srt)
-    tmp_file = splitext(basename(test_video))[0] + splitext(downloaded_srt)[1]
-    copyfile(xmp_downloader.get_downloaded_subtile(), tmp_file)
+    tmp_file = splitext(basename(video_filename))[0] + splitext(downloaded_srt)[1]
+    copyfile(xmp_downloader.get_downloaded_subtile(), os.path.dirname(os.path.realpath(video_filename))+ "//" +tmp_file)
+    xmp_downloader.kill_xmp_player()
     convertFile(tmp_file)
+
+
+def main() :
+    print "main function"
+    # download_subtitle(test_video)
+    # download_subtitle_in_dir("c:\\test_dvd\\")
+    download_subtitle_in_dir(parse_dir())
     print "done!"
 
 if __name__ == "__main__":
